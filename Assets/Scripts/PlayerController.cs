7977809930dc7eafc148757d278c653bfd18be9d
RICCOs <agaki08@internet.ru>
2023-06-05 17:31:10 +0300

    PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _horizontal;
+    private bool _isFacingRight = true;
+    //private bool _isFacingLeft = true;
+
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpingPower;
+
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
+
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpingPower);
+        }
+
+        if(Input.GetButtonUp("Jump") && _rb.velocity.y > 0f)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * 0.5f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(_horizontal * _speed, _rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer);
+    }
+
+    private void Flip()
+    {
+        if(_isFacingRight && _horizontal < 0f || !_isFacingRight && _horizontal > 0f)
+        {
+            _isFacingRight = !_isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
